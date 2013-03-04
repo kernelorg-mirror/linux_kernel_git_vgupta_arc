anthony-kolesov/arc_linux

ARC: ARCAngel4 touchscreen driver
-contributed by Pavel Sokolov

Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

include/linux/serio.h
@@ -198,4 +198,5 @@ static inline void serio_continue_rx(struct serio *serio)
 #define SERIO_DYNAPRO	0x3a
 #define SERIO_HAMPSHIRE	0x3b
 
+#define SERIO_DMC_FIT10	0x3c
 #endif

kernelorg-mirror/linux_kernel_git_vgupta_arc

include/uapi/linux/serio.h
@@ -76,5 +76,6 @@
 #define SERIO_HAMPSHIRE	0x3b
 #define SERIO_PS2MULT	0x3c
 #define SERIO_TSC40	0x3d
+#define SERIO_DMC_FIT10 0x3e
 
 #endif /* _UAPI_SERIO_H */