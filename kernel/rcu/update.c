3bad1a6c439380b3580c8e78e6754f029d09a791
Vineet Gupta <vgupta@synopsys.com>
2017-07-10 11:52:15 -0700

    xxx: debug: supres RCU stall detection
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- kernel/rcu/update.c
+++ kernel/rcu/update.c
@@ -442,7 +442,7 @@ EXPORT_SYMBOL_GPL(do_trace_rcu_torture_read);
 #define RCU_STALL_DELAY_DELTA	       0
 #endif
 
-int rcu_cpu_stall_suppress __read_mostly; /* 1 = suppress stall warnings. */
+int rcu_cpu_stall_suppress __read_mostly = 1; /* 1 = suppress stall warnings. */
 EXPORT_SYMBOL_GPL(rcu_cpu_stall_suppress);
 static int rcu_cpu_stall_timeout __read_mostly = CONFIG_RCU_CPU_STALL_TIMEOUT;
 
