4e91373f3fb02c4607c31754d7c7ebe622951c7a
Vineet Gupta <vgupta@synopsys.com>
2019-04-11 16:26:46 -0700

    ARC: string: opencode memzero
    Gets rids of a branch (at the cost of extra scratch reg at call site).
    But all scratch regs are already rendered stale due to the call anyhow
    so this doesn't change anything.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/string.h
+++ arch/arc/include/asm/string.h
@@ -26,11 +26,14 @@ extern void *memset(void *ptr, int, __kernel_size_t);
 #define memset(p, c, sz)	__builtin_memset(p, c, sz)
 
 extern void *memcpy(void *, const void *, __kernel_size_t);
-extern void memzero(void *ptr, __kernel_size_t n);
 extern int memcmp(const void *, const void *, __kernel_size_t);
 extern char *strchr(const char *s, int c);
 extern char *strcpy(char *dest, const char *src);
 extern int strcmp(const char *cs, const char *ct);
 extern __kernel_size_t strlen(const char *);
+
+static inline void memzero(void *ptr, __kernel_size_t n){
+	memset(ptr, 0, n);
+}
 
 #endif /* _ASM_ARC_STRING_H */
