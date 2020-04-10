4bbe71f555774e71e0baed77d3e2eaed9ceea028
Vineet Gupta <vgupta@synopsys.com>
2020-04-10 14:14:53 -0700

    ARC: pt_regs: remove ZOL
     - Entry code (interrupts/exceptions) need not save/restore ZOL regs
    
     - Any userspace ZOL references (ptrace, signal frame, process start)
       reworked such that ZOL regs are Zero-on-read, Ingore-on-write since
       the ptrace ABI need not change
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/asm-offsets.c
+++ arch/arc/kernel/asm-offsets.c
@@ -63,8 +63,10 @@ int main(void)
 	DEFINE(PT_ret, offsetof(struct pt_regs, ret));
 	DEFINE(PT_blink, offsetof(struct pt_regs, blink));
 	OFFSET(PT_fp, pt_regs, fp);
+#ifndef CONFIG_ARC_LACKS_ZOL
 	DEFINE(PT_lpe, offsetof(struct pt_regs, lp_end));
 	DEFINE(PT_lpc, offsetof(struct pt_regs, lp_count));
+#endif
 #ifdef CONFIG_ISA_ARCV2
 	OFFSET(PT_r12, pt_regs, r12);
 	OFFSET(PT_r30, pt_regs, r30);
