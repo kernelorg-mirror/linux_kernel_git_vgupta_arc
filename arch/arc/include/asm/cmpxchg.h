yaroslavsadin/linux

ARCv3: cmpxchg/xchg based on LLOCKL/SCONDL/BRccL instructions
Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

arch/arc/include/asm/cmpxchg.h
@@ -38,6 +38,37 @@
 	_prev;								\
 })
 
+#ifdef CONFIG_64BIT
+
+#define __cmpxchg64_relaxed(ptr, old, new)				\
+({									\
+	__typeof__(*(ptr)) __prev;					\
+									\
+	__asm__ __volatile__(						\
+	"1:	llockl  %0, [%1]	\n"				\
+	"	brnel   %0, %2, 2f	\n"				\
+	"	scondl  %3, [%1]	\n"				\
+	"	bnz     1b		\n"				\
+	"2:				\n"				\
+	: "=&r"(__prev)							\
+	: "r"(ptr),							\
+	  "ir"(old),						\
+	  "r"(new)							\
+	: "cc",								\
+	  "memory");							\
+									\
+	__prev;								\
+})
+
+#else
+
+#define __cmpxchg64_relaxed(ptr, old, new)				\
+({									\
+	BUILD_BUG();							\
+	(__typeof__(*(ptr))) -1UL;					\
+})
+#endif
+
 #define cmpxchg(ptr, old, new)					        \
 ({									\
 	__typeof__(ptr) _p_ = (ptr);					\
@@ -52,6 +83,11 @@
 	         */							\
 		smp_mb();						\
 		_prev_ = __cmpxchg_relaxed(_p_, _o_, _n_);		\
+		smp_mb();						\
+		break;							\
+	case 8:								\
+		smp_mb();						\
+		_prev_ = __cmpxchg64_relaxed(_p_, _o_, _n_);		\
 		smp_mb();						\
 		break;							\
 	default:							\
@@ -70,6 +106,9 @@
 	switch(sizeof((_p_))) {				        	\
 	case 4:								\
 		_prev_ = __cmpxchg_relaxed(_p_, _o_, _n_);		\
+		break;							\
+	case 8:								\
+		_prev_ = __cmpxchg64_relaxed(_p_, _o_, _n_);	\
 		break;							\
 	default:							\
 		BUILD_BUG();						\
@@ -150,6 +189,27 @@
 	_val_;		/* get old value */				\
 })
 
+#ifdef CONFIG_64BIT
+
+#define __xchg64_relaxed(ptr, val)					\
+({									\
+	__asm__ __volatile__(						\
+	"	exl  %0, [%1]	\n"					\
+	: "+r"(val)							\
+	: "r"(ptr)							\
+	: "memory");							\
+	_val_;								\
+})
+
+#else
+
+#define __xchg64_relaxed(ptr, val)					\
+({									\
+	BUILD_BUG();							\
+	(__typeof__(*(ptr))) -1UL;					\
+})
+#endif
+
 #define xchg(ptr, val)							\
 ({									\
 	__typeof__(ptr) _p_ = (ptr);					\
@@ -159,6 +219,11 @@
 	case 4:								\
 		smp_mb();						\
 		_val_ = __xchg_relaxed(_p_, _val_);			\
+	        smp_mb();						\
+		break;							\
+	case 8:								\
+		smp_mb();						\
+		_val_ = __xchg64_relaxed(_p_, _val_);			\
 	        smp_mb();						\
 		break;							\
 	default:							\
@@ -175,6 +240,9 @@
 	switch(sizeof(*(_p_))) {					\
 	case 4:								\
 		_val_ = __xchg_relaxed(_p_, _val_);			\
+		break;							\
+	case 8:								\
+		_val_ = __xchg64_relaxed(_p_, _val_);			\
 		break;							\
 	default:							\
 		BUILD_BUG();						\

kernelorg-mirror/linux_kernel_git_vgupta_arc

arch/arc/include/asm/cmpxchg.h
@@ -38,6 +38,38 @@
 	_prev;								\
 })
 
+#ifdef CONFIG_64BIT
+
+#define __cmpxchg64(ptr, old, new)					\
+({									\
+	__typeof__(*(ptr)) __prev;					\
+									\
+	__asm__ __volatile__(						\
+	"1:	llockl  %0, [%1]	\n"				\
+	"	brnel   %0, %2, 2f	\n"				\
+	"	scondl  %3, [%1]	\n"				\
+	"	bnz     1b		\n"				\
+	"2:				\n"				\
+	: "=&r"(__prev)							\
+	: "r"(ptr),							\
+	  "ir"(old),							\
+	  "r"(new)							\
+	: "cc",								\
+	  "memory");							\
+									\
+	__prev;								\
+})
+
+#else
+
+#define __cmpxchg64(ptr, old, new)					\
+({									\
+	BUILD_BUG();							\
+	(__typeof__(*(ptr))) -1UL;					\
+})
+
+#endif
+
 #define arch_cmpxchg_relaxed(ptr, old, new)				\
 ({									\
 	__typeof__(ptr) _p_ = (ptr);					\
@@ -49,13 +81,16 @@
 	case 4:								\
 		_prev_ = __cmpxchg(_p_, _o_, _n_);			\
 		break;							\
+	case 8:								\
+		_prev_ = __cmpxchg64(_p_, _o_, _n_);			\
+		break;							\
 	default:							\
 		BUILD_BUG();						\
 	}								\
 	_prev_;								\
 })
 
-#else
+#else  /* !CONFIG_ARC_HAS_LLSC */
 
 #define arch_cmpxchg(ptr, old, new)				        \
 ({									\
@@ -95,6 +130,28 @@
 	_val_;		/* get old value */				\
 })
 
+#ifdef CONFIG_64BIT
+
+#define __xchg64(ptr, val)						\
+({									\
+	__asm__ __volatile__(						\
+	"	exl  %0, [%1]	\n"					\
+	: "+r"(val)							\
+	: "r"(ptr)							\
+	: "memory");							\
+	_val_;								\
+})
+
+#else
+
+#define __xchg64(ptr, val)						\
+({									\
+	BUILD_BUG();							\
+	(__typeof__(*(ptr))) -1UL;					\
+})
+
+#endif
+
 #define arch_xchg_relaxed(ptr, val)					\
 ({									\
 	__typeof__(ptr) _p_ = (ptr);					\
@@ -103,6 +160,9 @@
 	switch(sizeof(*(_p_))) {					\
 	case 4:								\
 		_val_ = __xchg(_p_, _val_);				\
+		break;							\
+	case 8:								\
+		_val_ = __xchg64(_p_, _val_);				\
 		break;							\
 	default:							\
 		BUILD_BUG();						\